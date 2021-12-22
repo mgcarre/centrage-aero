58264a8037f0bb4df425f3de09996ad639aa24f2
mgcarre <mathgb@gmail.com>
2021-12-22 20:12:49 +0100

    maj code JS + Class

--- services/web/prepavol/prepavol/static/prepflight.js
+++ services/web/prepavol/prepavol/static/prepflight.js
@@ -1,24 +1,98 @@
-let callsign;
-let bew = 0;
-let frontweight = 0;
-let rearweight = 0;
-let bagweight = 0;
-let bagweight2 = 0;
-let mainfuelweight = 0;
-let wingfuelweight = 0;
-let auxfuelweight = 0;
-let auw;
-let bemoment = 0;
-let frontmoment = 0;
-let rearmoment = 0;
-let bagmoment = 0;
-let bagmoment2 = 0;
-let mainfuelmoment = 0;
-let wingfuelmoment = 0;
-let auxfuelmoment = 0;
-let moment;
-let cg;
-let essence;
+class ACFT {
+  constructor () {
+    this.callsign = ""
+    this.bew = 0
+    this.frontweight = 0
+    this.rearweight = 0
+    this.bagweight = 0
+    this.bagarm = 0
+    this.bagweight2 = 0
+    this.bagarm2 = 0
+    this.mainfuelweight = 0
+    this.wingfuelweight = 0
+    this.auxfuelweight = 0
+    this.bearm = 0
+    this.mainfuelarm = 0
+    this.wingfuelarm = 0
+    this.auxfuelarm = 0
+  }
+  get auw() {
+    return [
+      this.bew,
+      this.frontweight,
+      this.rearweight,
+      this.bagweight,
+      this.bagweight2,
+      this.mainfuelweight,
+      this.wingfuelweight,
+      this.auxfuelweight,
+    ].reduce((a, b) => a + b)
+  }
+  get moment() {
+    return [
+      this.bemoment,
+      this.frontmoment,
+      this.rearmoment,
+      this.bagmoment,
+      this.bagmoment2,
+      this.mainfuelmoment,
+      this.wingfuelmoment,
+      this.auxfuelmoment,
+    ].reduce((a, b) => a + b)
+  }
+  get cg() {
+    return this.moment / this.auw
+  }
+  get bemoment() {
+    return this.bew * this.bearm
+  }
+  get frontmoment() {
+    return this.frontweight * this.frontarm
+  }
+  get rearmoment() {
+    return this.rearweight * this.reararm
+  }
+  get bagmoment() {
+    return this.bagweight * this.bagarm
+  }
+  get bagmoment2() {
+    return this.bagweight2 * this.bagarm2
+  }
+  get mainfuelmoment() {
+    return this.mainfuelweight * this.mainfuelarm
+  }
+  get wingfuelmoment() {
+    return this.wingfuelweight * this.wingfuelarm
+  }
+  get auxfuelmoment() {
+    return this.auxfuelweight * this.auxfuelarm
+  }
+}
+class ACFTParams {
+  constructor () {
+    this.arms = null
+    this.bagmax = 0
+    this.bagmax2 = 0
+    this.bew = 0
+    this.envelope = []
+    this.fuel_name = ""
+    this.fuelrate = 0
+    this.maxauxfuel = 0
+    this.maxmainfuel = 0
+    this.maxwingfuel = 0
+    this.mtow = 0
+    this.planetype = ""
+    this.sumbagmax = 0
+    this.unusable_mainfuel = 0
+    this.unusable_wingfuel = 0
+  }
+  populate(elem) {
+    Object.assign(this, elem)
+  }
+}
+let avion = new ACFT()
+let params = new ACFTParams()
+let essence
 
 function reset_form() {
   const selectfields = [
@@ -40,32 +114,9 @@ function reset_form() {
 }
 
 function update_totals() {
-  auw = [
-    bew,
-    frontweight,
-    rearweight,
-    bagweight,
-    bagweight2,
-    mainfuelweight,
-    wingfuelweight,
-    auxfuelweight,
-  ].reduce((a, b) => a + b);
-
-  moment = [
-    bemoment,
-    frontmoment,
-    rearmoment,
-    bagmoment,
-    bagmoment2,
-    mainfuelmoment,
-    wingfuelmoment,
-    auxfuelmoment,
-  ].reduce((a, b) => a + b);
-  cg = moment / auw;
-
-  update_element_HTML("#auw", auw.toFixed(1), "Kg")
-  update_element_HTML("#cg", cg.toFixed(2), "m")
-  update_element_HTML("#moment", moment.toFixed(0), "Kg.m")
+  update_element_HTML("#auw", avion.auw.toFixed(1), "Kg")
+  update_element_HTML("#cg", avion.cg.toFixed(2), "m")
+  update_element_HTML("#moment", avion.moment.toFixed(0), "Kg.m")
 }
 
 function update_element_HTML(elementId, value, unit = "") {
@@ -73,20 +124,20 @@ function update_element_HTML(elementId, value, unit = "") {
 }
 
 async function update_plane() {
-  callsign = document.querySelector("#callsign").value;
-  plane = planes[callsign];
+  avion = new ACFT()
+  params = new ACFTParams()
+  avion.callsign = document.querySelector("#callsign").value;
+  params.populate(planes[avion.callsign])
 
-  req = await fetch(`essence?type=${plane.fuel_name}`)
+  const req = await fetch(`essence?type=${params.fuel_name}`)
   essence = await req.json()
-  planetype = plane.planetype;
-  bew = plane.bew;
-  bearm = plane.arms.bew;
-  bemoment = bew * bearm;
+  avion.bew = params.bew;
+  avion.bearm = params.arms.bew;
 
-  update_element_HTML("#planetype", planetype)
-  update_element_HTML("#bew", bew.toFixed(0), "Kg")
-  update_element_HTML("#bearm", bearm.toFixed(2), "m")
-  update_element_HTML("#bemoment", bemoment.toFixed(0), "Kg.m")
+  update_element_HTML("#planetype", params.planetype)
+  update_element_HTML("#bew", avion.bew.toFixed(0), "Kg")
+  update_element_HTML("#bearm", avion.bearm.toFixed(2), "m")
+  update_element_HTML("#bemoment", avion.bemoment.toFixed(0), "Kg.m")
 
   update_front();
   update_rear();
@@ -102,16 +153,12 @@ function update_front() {
   const w1 = parseInt(document.querySelector("#pax0").value);
   const w2 = parseInt(document.querySelector("#pax1").value);
 
-  if (w1 > 0) {
-    document.querySelector(".errors").innerHTML = ""
-  }
-  frontweight = w1 + w2;
-  frontarm = plane.arms.front;
-  frontmoment = frontweight * frontarm;
+  avion.frontweight = w1 + w2;
+  avion.frontarm = params.arms.front;
 
-  update_element_HTML("#frontweight", frontweight.toFixed(0), "Kg")
-  update_element_HTML("#frontarm", frontarm.toFixed(2), "m");
-  update_element_HTML("#frontmoment", frontmoment.toFixed(0), "Kg.m");
+  update_element_HTML("#frontweight", avion.frontweight.toFixed(0), "Kg")
+  update_element_HTML("#frontarm", avion.frontarm.toFixed(2), "m");
+  update_element_HTML("#frontmoment", avion.frontmoment.toFixed(0), "Kg.m");
 
   update_totals();
 }
@@ -119,61 +166,57 @@ function update_front() {
 function update_rear() {
   const w1 = parseInt(document.querySelector("#pax2").value);
   const w2 = parseInt(document.querySelector("#pax3").value);
-  if (plane.arms.rear == 0) {
+  if (params.arms.rear == 0) {
     document.querySelector("#pax2").selectedIndex = 0
     document.querySelector("#pax3").selectedIndex = 0
   }
-  rearweight = w1 + w2;
-  reararm = plane.arms.rear;
-  rearmoment = rearweight * reararm;
+  avion.rearweight = w1 + w2;
+  avion.reararm = params.arms.rear;
 
-  update_element_HTML("#rearweight", rearweight.toFixed(0), "Kg");
-  update_element_HTML("#reararm", reararm.toFixed(2), "m");
-  update_element_HTML("#rearmoment", rearmoment.toFixed(0), "Kg.m");
+  update_element_HTML("#rearweight", avion.rearweight.toFixed(0), "Kg");
+  update_element_HTML("#reararm", avion.reararm.toFixed(2), "m");
+  update_element_HTML("#rearmoment", avion.rearmoment.toFixed(0), "Kg.m");
 
   update_totals();
 }
 
 function update_baggage() {
-  bagweight = parseInt(document.querySelector("#baggage").value);
-  bagarm = plane.arms.baggage;
-  bagmoment = bagweight * bagarm;
+  avion.bagweight = parseInt(document.querySelector("#baggage").value);
+  avion.bagarm = params.arms.baggage;
 
-  update_element_HTML("#bagweight", bagweight.toFixed(0), "Kg");
-  update_element_HTML("#bagarm", bagarm.toFixed(2), "m");
-  update_element_HTML("#bagmoment", bagmoment.toFixed(0), "Kg.m");
+  update_element_HTML("#bagweight", avion.bagweight.toFixed(0), "Kg");
+  update_element_HTML("#bagarm", avion.bagarm.toFixed(2), "m");
+  update_element_HTML("#bagmoment", avion.bagmoment.toFixed(0), "Kg.m");
 
   update_totals();
 }
 
 function update_baggage2() {
-  bagweight2 = parseInt(document.querySelector("#baggage2").value);
-  if (plane.bagmax2 == 0) {
+  avion.bagweight2 = parseInt(document.querySelector("#baggage2").value);
+  if (params.bagmax2 == 0) {
     document.querySelector("#baggage2").selectedIndex = 0
-    bagweight2 = 0;
+    avion.bagweight2 = 0;
   }
-  bagarm2 = plane.arms.baggage2;
-  bagmoment2 = bagweight2 * bagarm2;
+  avion.bagarm2 = params.arms.baggage2;
 
-  update_element_HTML("#bagweight2", bagweight2.toFixed(0), "Kg");
-  update_element_HTML("#bagarm2", bagarm2.toFixed(2), "m");
-  update_element_HTML("#bagmoment2", bagmoment2.toFixed(0), "Kg.m");
+  update_element_HTML("#bagweight2", avion.bagweight2.toFixed(0), "Kg");
+  update_element_HTML("#bagarm2", avion.bagarm2.toFixed(2), "m");
+  update_element_HTML("#bagmoment2", avion.bagmoment2.toFixed(0), "Kg.m");
 
   update_totals();
 }
 
 function update_mainfuel() {
   const value = parseFloat(document.querySelector("#mainfuel").value);
-  if (plane.maxmainfuel == 0) {
+  if (params.maxmainfuel == 0) {
     document.querySelector("#mainfuel").selectedIndex = 0
   }
-  mainfuelweight = value * essence?.density;
-  mainfuelarm = plane.arms.mainfuel;
-  mainfuelmoment = mainfuelweight * mainfuelarm;
+  avion.mainfuelweight = value * essence?.density;
+  avion.mainfuelarm = params.arms.mainfuel;
 
-  update_element_HTML("#mainfuelmass", mainfuelweight.toFixed(0), "Kg");
-  update_element_HTML("#mainfuelarm", mainfuelarm.toFixed(2), "m");
-  update_element_HTML("#mainfuelmoment", mainfuelmoment.toFixed(0), "Kg.m");
+  update_element_HTML("#mainfuelmass", avion.mainfuelweight.toFixed(0), "Kg");
+  update_element_HTML("#mainfuelarm", avion.mainfuelarm.toFixed(2), "m");
+  update_element_HTML("#mainfuelmoment", avion.mainfuelmoment.toFixed(0), "Kg.m");
 
   update_totals();
 }
@@ -181,17 +224,16 @@ function update_mainfuel() {
 function update_wingfuel() {
   const w1 = parseInt(document.querySelector("#leftwingfuel").value);
   const w2 = parseInt(document.querySelector("#rightwingfuel").value);
-  if (plane.maxwingfuel == 0) {
+  if (params.maxwingfuel == 0) {
     document.querySelector("#leftwingfuel").selectedIndex = 0
     document.querySelector("#rightwingfuel").selectedIndex = 0
   }
-  wingfuelweight = (w1 + w2) * essence?.density;
-  wingfuelarm = plane.arms.wingfuel;
-  wingfuelmoment = wingfuelweight * wingfuelarm;
+  avion.wingfuelweight = (w1 + w2) * essence?.density;
+  avion.wingfuelarm = params.arms.wingfuel;
 
-  update_element_HTML("#wingfuelweight", wingfuelweight.toFixed(0), "Kg");
-  update_element_HTML("#wingfuelarm", wingfuelarm.toFixed(2), "m");
-  update_element_HTML("#wingfuelmoment", wingfuelmoment.toFixed(0), "Kg.m");
+  update_element_HTML("#wingfuelweight", avion.wingfuelweight.toFixed(0), "Kg");
+  update_element_HTML("#wingfuelarm", avion.wingfuelarm.toFixed(2), "m");
+  update_element_HTML("#wingfuelmoment", avion.wingfuelmoment.toFixed(0), "Kg.m");
 
   update_totals();
 }
@@ -199,29 +241,19 @@ function update_wingfuel() {
 function update_auxfuel() {
   const value = parseInt(document.querySelector("#auxfuel").value);
 
-  if (plane.maxauxfuel == 0) {
+  if (params.maxauxfuel == 0) {
     document.querySelector("#auxfuel").selectedIndex = 0
   }
-  auxfuelweight = value * 0.72;
-  auxfuelarm = plane.arms.auxfuel;
-  auxfuelmoment = auxfuelweight * auxfuelarm;
+  avion.auxfuelweight = value * essence?.density;
+  avion.auxfuelarm = params.arms.auxfuel;
 
-  update_element_HTML("#auxfuelmass", auxfuelweight.toFixed(0), "Kg");
-  update_element_HTML("#auxfuelarm", auxfuelarm.toFixed(2), "m");
-  update_element_HTML("#auxfuelmoment", auxfuelmoment.toFixed(0), "Kg.m");
+  update_element_HTML("#auxfuelmass", avion.auxfuelweight.toFixed(0), "Kg");
+  update_element_HTML("#auxfuelarm", avion.auxfuelarm.toFixed(2), "m");
+  update_element_HTML("#auxfuelmoment", avion.auxfuelmoment.toFixed(0), "Kg.m");
 
   update_totals();
 }
 
-// Initialization of the form
-update_plane();
-update_front();
-update_rear();
-update_baggage();
-update_baggage2();
-update_mainfuel();
-update_wingfuel();
-update_auxfuel();
 
 // Event callbacks
 const elem = new Map()
@@ -244,6 +276,14 @@ window.addEventListener("DOMContentLoaded", e => {
     e.preventDefault()
     reset_form()
   })
+  update_plane();
+  update_front();
+  update_rear();
+  update_baggage();
+  update_baggage2();
+  update_mainfuel();
+  update_wingfuel();
+  update_auxfuel();
 })
 
 

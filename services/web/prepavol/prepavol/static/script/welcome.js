aaf21e2622ec4b3d7e1a2d824d34daabb54bb51e
mgcarre <mathgb@gmail.com>
2022-03-04 20:26:53 +0100

    affichage station metar + ajout stations

--- services/web/prepavol/prepavol/static/script/welcome.js
+++ services/web/prepavol/prepavol/static/script/welcome.js
@@ -2,11 +2,26 @@ document.querySelectorAll("button").forEach(btn => {
     btn.addEventListener("click", e => {
         e.preventDefault()
         e.stopPropagation()
+        btn.classList.add("is-loading")
+        toggleSelectedButton(btn)
         const ad = e.target.innerText
         fetch(`/metar/${ad}`)
             .then(r => r.json())
             .catch(err => console.error(err))
-            .then(r => alert(r.metar))
+            .then(r => {
+                document.getElementById("metar_data").innerHTML = r.metar
+                btn.classList.remove("is-loading")
+            })
             .catch(err => console.error(err))
     })
 })
+
+function toggleSelectedButton(btn) {
+    document.querySelectorAll("button").forEach(button => {
+        if (button !== btn) {
+            button.classList.remove("is-selected", "is-info")
+        } else {
+            button.classList.add("is-selected", "is-info")
+        }
+    })
+}
